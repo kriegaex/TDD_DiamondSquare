0eddc151377d141d05707751df55a685ed3ce287
Alexander Kriegisch <Alexander@Kriegisch.name>
2017-01-28 15:13:13 +0100

    Make test for correctly initialised corners pass

--- src/main/java/de/scrum_master/tdd/Tile.java
+++ src/main/java/de/scrum_master/tdd/Tile.java
@@ -1,18 +1,27 @@
 package de.scrum_master.tdd;
 
+import java.util.Arrays;
 import java.util.Random;
 
 class Tile {
   private static final Random RANDOM = new Random();
 
   private int sizeOrder;
+  private int edgeLength;
+  private float[][] matrix;
 
   public Tile(int sizeOrder) {
     this.sizeOrder = sizeOrder;
+    edgeLength = (int) (Math.pow(2, sizeOrder) + 1);
+    matrix = new float[edgeLength][edgeLength];
   }
 
   public Tile(int sizeOrder, float bottomLeft, float bottomRight, float topLeft, float topRight) {
     this(sizeOrder);
+    matrix[0][0] = bottomLeft;
+    matrix[edgeLength - 1][0] = bottomRight;
+    matrix[0][edgeLength - 1] = topLeft;
+    matrix[edgeLength - 1][edgeLength - 1] = topRight;
   }
 
   static float average(float... corners) {
@@ -27,10 +36,13 @@ class Tile {
   }
 
   public int getEdgeLength() {
-    return (int) (Math.pow(2, sizeOrder) + 1);
+    return edgeLength;
   }
 
   public float[][] toArray() {
-    return new float[getEdgeLength()][getEdgeLength()];
+    final float[][] result = new float[edgeLength][];
+    for (int i = 0; i < edgeLength; i++)
+      result[i] = Arrays.copyOf(matrix[i], edgeLength);
+    return result;
   }
 }
