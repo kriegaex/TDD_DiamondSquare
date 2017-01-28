f852c740f5322193ced17f3a48d85080cabe4e0b
Alexander Kriegisch <Alexander@Kriegisch.name>
2017-01-28 13:10:15 +0100

    New static method Tile.average + failing test
    This method is meant to calculate the arithmetic average for an
    arbitrary number of floating-point arguments.

--- src/main/java/de/scrum_master/tdd/Tile.java
+++ src/main/java/de/scrum_master/tdd/Tile.java
@@ -1,10 +1,16 @@
 package de.scrum_master.tdd;
 
+import java.util.List;
+
 class Tile {
   private int sizeOrder;
 
   Tile(int sizeOrder) {
     this.sizeOrder = sizeOrder;
+  }
+
+  public static float average(float... corners) {
+    return -999999;
   }
 
   public int getEdgeLength() {
--- src/test/groovy/de/scrum_master/tdd/TileTest.groovy
+++ src/test/groovy/de/scrum_master/tdd/TileTest.groovy
@@ -29,6 +29,26 @@ class TileTest extends Specification {
     10        | 1025
   }
 
+  @Unroll
+  def "average of #corners is #average"(float[] corners, float average) {
+    expect:
+    Tile.average(corners) == average
+
+    where:
+    corners                  | average
+    [0, 0, 0, 0]             | 0
+    [5, 5, 5, 5]             | 5
+    [1, 2, 2, 1]             | 1.5
+    [1, 2, 3, 4]             | 2.5
+    [-2, 2, -3, 3]           | 0
+    [1, 2, 3]                | 2
+    [1, 2]                   | 1.5
+    [3]                      | 3
+    [12, 34, 56, 78]         | 45
+    [-11, 22, -33, 44]       | 5.5
+    [1.23, 4.56, 7.89, 0.12] | 3.45
+  }
+
   private static int log2(int number) {
     log(number) / log(2)
   }
