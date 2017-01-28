0d3af823ed54afcd52d92e65ff228bd13ada8c35
Alexander Kriegisch <Alexander@Kriegisch.name>
2017-01-28 12:28:22 +0100

    Extend edge length test, still passing
    Test 2^n + 1 relationship via reverse function log2 and make the
    relationship more explicit in the test name.

--- src/test/groovy/de/scrum_master/tdd/TileTest.groovy
+++ src/test/groovy/de/scrum_master/tdd/TileTest.groovy
@@ -3,14 +3,17 @@ package de.scrum_master.tdd
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static java.lang.Math.log
+
 class TileTest extends Specification {
   @Unroll
-  def "tile of size order #sizeOrder has square edge length #edgeLength"() {
+  def "tile of size order #sizeOrder has square edge length 2^#sizeOrder + 1 = #edgeLength"() {
     given:
     def tile = new Tile(sizeOrder)
 
     expect:
     tile.getEdgeLength() == edgeLength
+    log2(edgeLength - 1) == sizeOrder
 
     where:
     sizeOrder | edgeLength
@@ -24,5 +27,9 @@ class TileTest extends Specification {
     8         | 257
     9         | 513
     10        | 1025
+  }
+
+  private static int log2(int number) {
+    log(number) / log(2)
   }
 }
