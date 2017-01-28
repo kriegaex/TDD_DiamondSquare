93caaaa497ae2c1f5a817d5b3ed9e879825b8b74
Alexander Kriegisch <Alexander@Kriegisch.name>
2017-01-28 12:00:34 +0100

    Make edge length test pass

--- src/main/java/de/scrum_master/tdd/Tile.java
+++ src/main/java/de/scrum_master/tdd/Tile.java
@@ -1,10 +1,13 @@
 package de.scrum_master.tdd;
 
 class Tile {
+  private int sizeOrder;
+
   Tile(int sizeOrder) {
+    this.sizeOrder = sizeOrder;
   }
 
   public int getEdgeLength() {
-    return 0;
+    return (int) (Math.pow(2, sizeOrder) + 1);
   }
 }
