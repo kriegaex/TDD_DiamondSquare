02fa48eda9e1750fb41a1a92489ce28d2c4e9af0
Alexander Kriegisch <Alexander@Kriegisch.name>
2017-02-20 15:24:06 +0100

    Simplify RestartBrowserIT
    ... so as to only show the restart trick

--- src/test/groovy/de/scrum_master/tdd/RestartBrowserIT.groovy
+++ src/test/groovy/de/scrum_master/tdd/RestartBrowserIT.groovy
@@ -12,33 +12,12 @@ import spock.lang.Shared
  */
 class RestartBrowserIT extends GebReportingSpec {
   def "Search web site Scrum-Master.de"() {
-    setup:
-    def deactivateAutoComplete =
-      "document.getElementById('mod_search_searchword')" +
-      ".setAttribute('autocomplete', 'off')"
-    def regexNumberOfMatches = /Insgesamt wurden ([0-9]+) Ergebnisse gefunden/
-
     when:
     go "https://scrum-master.de"
     report "welcome page"
 
     then:
     $("h2").text().startsWith("Herzlich Willkommen bei Scrum-Master.de")
-
-    when:
-    js.exec(deactivateAutoComplete)
-    $("form").searchword = "Product Owner" + Keys.ENTER
-
-    then:
-    waitFor { $("form#searchForm") }
-
-    when:
-    report "search results"
-    def searchResultSummary = $("form#searchForm").$("table.searchintro").text()
-    def numberOfMatches = (searchResultSummary =~ regexNumberOfMatches)[0][1] as int
-
-    then:
-    numberOfMatches > 0
 
     when: "browser is reset"
     resetBrowser()
